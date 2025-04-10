5d6a11ecba573ad885648c1fa09d277cf018ce01
Randi Øyri <randi.oyri@digdir.no>
2025-04-10 15:05:21 +0200

    generic description

--- src/main/java/no/idporten/metric/constants/MetricDescriptions.java
+++ src/main/java/no/idporten/metric/constants/MetricDescriptions.java
@@ -6,5 +6,6 @@ public class MetricDescriptions {
     public final static String APP_EXCEPTION_REDIS_DESCRIPTION= "Counter for connection problems from application to Redis";
     public final static String APP_EXCEPTION_INTERNAL_API_DESCRIPTION= "Counter for API connection problems from application to another internal application in namespace";
     public final static String APP_EXCEPTION_EXTERNAL_API_DESCRIPTION= "Counter for API connection problems from application to external API";
+    public final static String APP_EXCEPTION_GENERIC_DESCRIPTION= "Counter for connection problems from application to somewhere else";
 
 }
