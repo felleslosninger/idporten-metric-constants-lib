a72362cfa8b2653465bfa09fd9d5b0acedae4edb
Randi Øyri <randi.oyri@digdir.no>
2025-04-10 15:20:56 +0200

    MP-722: Added details tag and generic description (#6)

--- src/main/java/no/idporten/metric/constants/MetricCategories.java
+++ src/main/java/no/idporten/metric/constants/MetricCategories.java
@@ -6,4 +6,5 @@ public class MetricCategories {
         public final static String EXCEPTION_TYPE = "exception_type";
         public final static String EID = "eid";
         public final static String OIDC_INTEGRATION = "oidc_integration";
+        public final static String DETAILS = "details";
 }
